cdf2a2c445d48fd6f3993a8b454ea954d051b6b6
Auknji <110452994+hssn-Shafiq@users.noreply.github.com>
2024-06-30 01:37:58 +0500

    bootstrap 5 added

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,6 +1,6 @@
 import * as Route from '@/constants/routes';
 import logo from '@/images/alaya-arts-removebg.png';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { FacebookFilled, FacebookOutlined } from "@ant-design/icons"
 import { Link } from 'react-router-dom';
@@ -15,12 +15,21 @@ const Footer = () => {
     Route.PRIVACY_POLICY
   ];
 
+  useEffect(() => {
+    var tooltipTriggerList = [].slice.call(
+      document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    );
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  }, [])
+
   return !visibleOnlyPath.includes(pathname) ? null : (
 
     <footer className="footer">
     <div className="container">
       <div className="row">
-        <div className="footer-col">
+        <div className="footer-col col-md-3">
           <h4>company</h4>
           <ul>
             <li>
@@ -34,7 +43,7 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="footer-col">
+        <div className="footer-col col-md-3">
           <h4>get help</h4>
           <ul>
             <li>
@@ -51,7 +60,7 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="footer-col">
+        <div className="footer-col col-md-3">
           <h4>online shop</h4>
           <ul>
             <li>
@@ -68,20 +77,20 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="footer-col">
+        <div className="footer-col col-md-3">
           <h4>follow us</h4>
           <div className="social-links" style={{marginLeft:"2em"}}>
-            <Link to="#" title='follow alaya arts on facebook'>
-              <i className="fab fa-facebook-f" />
+            <Link to="#" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Follow Alaya Arts on Facebook">
+              <i className="fab fa-facebook-f"  />
               {/* <FontAwesomeIcon icon="fa-brands fa-facebook-f" /> */}
             </Link>
-            <Link to="#" title='follow alaya arts on twitter'>
+            <Link to="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Follow Alaya Arts on Twitter">
               <i className="fab fa-twitter" />
             </Link>
-            <Link to="#" title='follow alaya arts on instagram'>
+            <Link to="#" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Follow Alaya Arts on Instagram">
               <i className="fab fa-instagram" />
             </Link>
-            <Link to="#" title='follow alaya arts on linkedin '>
+            <Link to="#" data-bs-toggle="tooltip" data-bs-placement="left" title="Follow Alaya Arts on Linkedin">
               <i className="fab fa-linkedin-in" />
             </Link>
           </div>
