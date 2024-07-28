0ace8f08fcf3f17517a53326aa06dc2b050e3e65
Shayan Zulfiqar <146065078+ShayanZulfiqar@users.noreply.github.com>
2024-07-28 12:40:39 +0400

    Shayan| responsive ImWtext

--- src/components/common/ImageWithText.jsx
+++ src/components/common/ImageWithText.jsx
@@ -7,14 +7,14 @@ const ImageWithText = ({ t1, t2, t3, desc, link, img, place }) => {
   return (
     <div className="banner mb-5" style={{ display: 'flex', flexDirection: place === 1 ? 'row' : 'row-reverse' }}>
       <div className="banner-desc" style={{ order: place }}>
-        <h1 className="text-thin">
+        <h1 className="text-thin" style={{ fontSize: "15px" }}>
           <strong>{t1}</strong>
           &nbsp;{t2}&nbsp;
           <strong>{t3}</strong>
         </h1>
-        <p>{desc}</p>
+        <p style={{ fontSize: "12px", lineHeight: "1.12" }}>{desc}</p>
         <br />
-        <Link to={link} className="button">
+        <Link to={link} className="button" style={{ fontSize: '10px' }}>
           Explore Now &nbsp;
           <ArrowRightOutlined />
         </Link>
--- src/components/product/ProductFeatured.jsx
+++ src/components/product/ProductFeatured.jsx
@@ -1,9 +1,9 @@
-import { ImageLoader } from '@/components/common';
-import PropType from 'prop-types';
-import React from 'react';
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
-import { useHistory } from 'react-router-dom';
-import { salesOff } from '@/helpers/utils';
+import { ImageLoader } from "@/components/common";
+import PropType from "prop-types";
+import React from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { useHistory } from "react-router-dom";
+import { salesOff } from "@/helpers/utils";
 const ProductFeatured = ({ product }) => {
   const history = useHistory();
   const onClickItem = () => {
@@ -14,39 +14,50 @@ const ProductFeatured = ({ product }) => {
 
   return (
     <SkeletonTheme color="#e1e1e1" highlightColor="#f2f2f2">
-      <div className="product-display" onClick={onClickItem} role="presentation">
+      <div
+        className="product-display"
+        onClick={onClickItem}
+        role="presentation"
+      >
         {salesOff(product.comparePrice, product.price) ? (
-            // <div className="sale_off">
-            //   <span>- {salesOff(product.comparePrice, product.price)}%</span>
-            // </div>
-        <div className='sale_off'>
-          <span style={{left:"auto", borderRadius:"0px 0px 25px 0px"}} className='p-3'>
-          - {salesOff(product.comparePrice, product.price)}%   
-          </span>
-         </div>
-
-          ) : (
-            ""
-          )}
+          // <div className="sale_off">
+          //   <span>- {salesOff(product.comparePrice, product.price)}%</span>
+          // </div>
+          <div className="sale_off">
+            <span
+              style={{ left: "auto", borderRadius: "0px 0px 25px 0px" }}
+              className="p-3"
+            >
+              - {salesOff(product.comparePrice, product.price)}%
+            </span>
+          </div>
+        ) : (
+          ""
+        )}
         <div className="product-display-img">
           {product.image ? (
-            <ImageLoader
-              className="product-card-img"
-              src={product.image}
-            />
-          ) : <Skeleton width="100%" height="100%" />}
+            <ImageLoader className="product-card-img" src={product.image} />
+          ) : (
+            <Skeleton width="100%" height="100%" />
+          )}
         </div>
         <div className="product-display-details">
           <h2>{product.name || <Skeleton width={80} />}</h2>
           <p className="text-subtle text-italic">
             {product.brand || <Skeleton width={40} />}
           </p>
-          <p className="text-subtle text-italic">
-          {product.price || <Skeleton width={40} />}
-          </p>
-          <p className="text-subtle text-italic">
-           <strike>{product.comparePrice ? product.comparePrice : "  " || <Skeleton width={40} />}</strike> 
-          </p>
+          <div className="d-flex justify-content-between" style={{marginTop: "20px"}}>
+            <p className="text-subtle text-italic">
+              {product.price || <Skeleton width={40} />}
+            </p>
+            <p className="text-subtle text-italic">
+              <strike>
+                {product.comparePrice
+                  ? product.comparePrice
+                  : "  " || <Skeleton width={40} />}
+              </strike>
+            </p>
+          </div>
         </div>
       </div>
     </SkeletonTheme>
@@ -58,8 +69,8 @@ ProductFeatured.propTypes = {
     image: PropType.string,
     name: PropType.string,
     id: PropType.string,
-    brand: PropType.string
-  }).isRequired
+    brand: PropType.string,
+  }).isRequired,
 };
 
 export default ProductFeatured;
